7faa2dcae8396fcc8f118406466a486cab206b0f
Peter Schenkels <43245077+Peter-Schenkels@users.noreply.github.com>
2019-01-18 01:34:22 +0100

    Update Vuilnis.py

--- Vuilnis.py
+++ Vuilnis.py
@@ -1,3 +1,135 @@
+import RPi.GPIO as GPIO
+import time
+import signal
+import sys
+import lcddriver
+
+
+while True:
+    lcd = lcddriver.lcd()
+    lcd.lcd_clear()
+
+
+    GPIO.setmode(GPIO.BCM)
+
+
+    pinTrigger1 = 18
+    pinEcho1 = 24
+    pinTrigger2 = 19
+    pinEcho2 = 26
+    
+    
+
+    def close(signal, frame):
+            
+            GPIO.cleanup() 
+            sys.exit(0)
+            lcd.lcd_clear()
+
+    signal.signal(signal.SIGINT, close)
+
+    # set GPIO input and output channels
+    
+    GPIO.setup(pinTrigger2, GPIO.OUT)
+    GPIO.setup(pinEcho2, GPIO.IN)
+    GPIO.setup(pinTrigger1, GPIO.OUT)
+    GPIO.setup(pinEcho1, GPIO.IN)
+
+    #1e sensor kalibratie
+    # Calibreren	
+    GPIO.output(pinTrigger1, True)	
+    time.sleep(0.00001)
+    GPIO.output(pinTrigger1, False)
+
+    startTijd = time.time()
+    stopTijd = time.time()
+
+    while 0 == GPIO.input(pinEcho1):
+        startTijd = time.time()
+
+            
+    while 1 == GPIO.input(pinEcho1):
+        stopTijd = time.time()
+
+            
+    TimeElapsed = stopTijd - startTijd
+        
+    afstand = (TimeElapsed * 34300) / 2
+
+    print ("Kalibratie waarde 1: %.1f cm" % afstand)
+    time.sleep(1)
+    if afstand < 400:
+        
+        
+        lcdWaarde = "Afstand: %.1f cm" % afstand
+        lcd.lcd_display_string(lcdWaarde ,1)
+        lcd.lcd_display_string("Kalibratie 1 volt", 2)
+        time.sleep(2)
+        K1= afstand
+        
+    
+    else:
+        lcd.lcd_display_string("Kalibratie 1 mislukt", 1)
+        lcd.lcd_display_string("Herstart in 2s", 2)
+        time.sleep(2)
+        continue
+    #2e sensor kalibratie
+    GPIO.output(pinTrigger2, True)	
+    time.sleep(0.00001)
+    GPIO.output(pinTrigger2, False)
+
+    startTijd = time.time()
+    stopTijd = time.time()
+
+    while 0 == GPIO.input(pinEcho2):
+        startTijd = time.time()
+
+            
+    while 1 == GPIO.input(pinEcho2):
+        stopTijd = time.time()
+
+            
+    TimeElapsed = stopTijd - startTijd
+        
+    afstand = (TimeElapsed * 34300) / 2
+
+    print ("Kalibratie waarde 2: %.1f cm" % afstand)
+    time.sleep(1)
+    if afstand < 400:
+        
+        lcdWaarde = "Afstand: %.1f cm" % afstand
+        lcd.lcd_display_string(lcdWaarde ,1)
+        lcd.lcd_display_string("Kalibratie 2 vol", 2)
+        time.sleep(2)
+        K2 = afstand
+        if (K2 - K1) < 5 and (K2 - K1) > -5:
+            diepte1 = K1
+            diepte2 = K2
+            break
+        else:
+            lcd.lcd_clear()
+            lcd.lcd_display_string("Verschil te hoog", 1)
+            time.sleep(2)
+            lcd.lcd_clear()
+            lcd.lcd_display_string("Sensor 1" + str(K1) + "cm", 1)
+            lcd.lcd_display_string("Sensor 2" + str(K2) + "cm", 2)
+            time.sleep(2)
+            lcd.lcd_clear()
+            lcd.lcd_display_string("Kalibratie mislukt", 1)
+            lcd.lcd_display_string("Herstart in 2s", 2)
+            time.sleep(2)
+            
+            
+            
+            
+    
+    else:
+        lcd.lcd_display_string("Kalibratie 2 mislukt", 1)
+        lcd.lcd_display_string("Herstart in 2s", 2)
+        time.sleep(2)
+                
+
+            
 while True:
 	#Sensor 1
 	GPIO.output(pinTrigger1, True)	
@@ -60,3 +192,5 @@ while True:
     
 	
 	time.sleep(2)
+	
+	
